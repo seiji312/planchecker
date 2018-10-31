42d19073972c1eee3da60895936d03aa5745547b
seiji312 <ss_vsuer_312@outlook.com>
2018-10-31 14:05:18 -0700

    Functionning  run check

--- Seiji.PlanChecker/ViewModels/MainViewModel.cs
+++ Seiji.PlanChecker/ViewModels/MainViewModel.cs
@@ -9,6 +9,8 @@ using ESAPIX.Common;
 using VMS.TPS.Common.Model.API;
 using System.Collections.ObjectModel;
 using Prism.Commands;
+using ESAPIX.Constraints.DVH;
+using System.Windows;
 
 namespace Seiji.PlanChecker.ViewModels
 {
@@ -18,7 +20,39 @@ namespace Seiji.PlanChecker.ViewModels
 
         public MainViewModel()
         {
-            
+            CreateConstraints();
+            EvaluateCommand = new DelegateCommand(() =>
+            {
+                foreach (var pc in Constraints)
+                {
+                    var result = VMS.GetValue(sc =>
+                    {
+                        //Check if we can constrain first
+                        var canConstrain = pc.Constraint.CanConstrain(sc.PlanSetup);
+                        //If not..report why
+                        if (!canConstrain.IsSuccess) { return canConstrain; }
+                        else
+                        {
+                            //Can constrain - so do it
+                            return pc.Constraint.Constrain(sc.PlanSetup);
+                        }
+                    });
+                    //Update UI
+                    pc.Result = result;
+                }
+            });
+        }
+
+        private void CreateConstraints()
+        {
+            Constraints.AddRange(new PlanConstraint[]
+            {
+                new PlanConstraint(ConstraintBuilder.Build("PTV", "Max[%] <= 110")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
+                new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
+                //new PlanConstraint(new CTDateConstraint())
+            });
         }
 
 
